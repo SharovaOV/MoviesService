9fe217de3bf0f5e78f8b8eee87978315f24ad716
SharovaOV <olyaart851@mail.ru>
2025-03-08 23:22:45 +0300

    Add ApiEndpoints

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -16,12 +16,12 @@ namespace Movies.Api.Controllers
             _movieRepository = movieRepository;
         }
 
-        [HttpPost("movies")]
+        [HttpPost(ApiEndpoints.Movies.Create)]
         public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
         {
             Movie movie = request.MapToMove();
             await _movieRepository.CreateAsync(movie);
-            return Created($"/api/movies/{movie.Id}", movie);
+            return Created($"{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
         }
 
     }
