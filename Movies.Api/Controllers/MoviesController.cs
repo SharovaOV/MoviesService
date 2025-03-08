d403a764d4dbbfe66009615e36334413a55b58fe
SharovaOV <olyaart851@mail.ru>
2025-03-08 23:02:49 +0300

    Add mapping

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
 using Movies.Application.Models;
 using Movies.Application.Repositories;
 using Movies.Contracts.Requests;
@@ -18,13 +19,7 @@ namespace Movies.Api.Controllers
         [HttpPost("movies")]
         public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
         {
-            Movie movie = new Movie
-            {
-                Id = Guid.NewGuid(),
-                Title = request.Title,
-                YearOfRelease = request.YearOfRelease,
-                Genres = request.Genres.ToList()
-            };
+            Movie movie = request.MapToMove();
             await _movieRepository.CreateAsync(movie);
             return Created($"/api/movies/{movie.Id}", movie);
         }
---
+++ Movies.Api/Mapping/ContractMapping.cs
@@ -0,0 +1,19 @@
+﻿using Movies.Application.Models;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Mapping
+{
+    public static class ContractMapping
+    {
+        public static Movie MapToMove(this CreateMovieRequest request)
+        {
+            return new Movie
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                YearOfRelease = request.YearOfRelease,
+                Genres = request.Genres.ToList()
+            };
+        }
+    }
+}
